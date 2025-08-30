df482c9a7d025e87cea60456e453e246f60d6a7c
SUHO PARK <duglikespiano@gmail.com>
2025-08-30 11:50:27 +0900

    ETC - BOILER PLATE JAVASCRIPT FILE ADDED

---
+++ app.js
@@ -0,0 +1,17 @@
+// Set dimensions and margins for the chart
+
+// Set up the x and y scales
+
+// Create the SVG element and append it to the chart container
+
+// Create a fake dataset
+
+// Define the x and y domains
+
+// Add the x-axis
+
+// Add the y-axis
+
+// Create the line generator
+
+// Add the line path to the SVG element
